ffc2fd65a95c19dcfa811f57fda032204282075f
Stanislav Simahin <stopikytub@gmail.com>
2021-06-28 23:39:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,12 @@ import discord
 import config
 from discord.ext import commands
 
-bot = commands.Bot(
-    command_prefix=config.bot_prefix, # bot prefix
-)
+
+bot = commands.Bot(command_prefix=config.bot_prefix) # bot prefix
 bot.remove_command("help") # remove standart help command (by discord.py)
 
 for cog in config.cogs: # get all cogs in config (cogs)
     bot.load_extension(cog) # load cogs.?
 
+    
 bot.run(config.bot_token) # run bot
